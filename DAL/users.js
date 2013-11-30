8834e3eaea20f9da1ece620da3be8750f1724636
odedc <odedcagan@gmail.com>
2013-11-30 13:47:14 +0200

    Update users.js

--- DAL/users.js
+++ DAL/users.js
@@ -11,3 +11,37 @@ exports.getUserFriends = function(id) {
     //TODO: implement
     return ['id1', 'id2'];
 };
+
+exports.getUserByName = function(req, res) 
+{
+	var userName = req.params.userName;
+	if (userName) 
+	{
+		db.collection('users', function(err, collection) 
+		{
+			collection.findOne({'name':userName}).toArray(function(err, items) 
+			{
+				res.send(items);
+			});
+		});
+	}
+};
+
+exports.addUser = function (req,res)
+{
+	var user = req.body;	
+	db.collection('users', function(err, collection) 
+	{
+		collection.insert(user, {safe:true}, function(err, result) {
+			if (err)
+			{
+				res.send({'error':'An error has occurred'});
+			} 
+			else
+			{				
+				res.send(result[0]);
+			}
+		});
+	});	
+}
+
