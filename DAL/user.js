006c942a9aa9964ee61836233d03b9343da69a51
odedc <odedcagan@gmail.com>
2013-12-01 23:47:14 +0200

    Update user.js

--- DAL/user.js
+++ DAL/user.js
@@ -69,32 +69,33 @@ exports.addPlace = function(user, place){
         {$push:{'places':{'_id':new BSON.ObjectID(place._id), 'name':place.name}}});
 };
 
-exports.getUserByName = function(userName) {
+exports.getUserByName = function(userName, callback) 
+{
 	db.collection('users', function(err, collection) 
 	{
 		collection.findOne({'name':userName}).toArray(function(err, user) 
 		{
-			return user;
+			if (err)
+				callback(err, null);
+			else			
+				callback(null, user;);
 		});
 	});	
 };
 
-exports.addUser= function (user)
+exports.addUser = function (user, callback)
 {	
 	db.collection('users', function(err, collection) 
 	{
-		collection.insert(user, {safe:true}, function(err, result) {
-			if (err)
-			{
-				return null;
-			} 
+		collection.insert(user, {safe:true}, function(err, result) 
+		{
+			if (err)			
+				callback(err, null);			
 			else
-			{				
-				return result[0];
-			}
+				callback(null, result[0]);
 		});
 	});	
-};
+}
 
 exports.getUser = getUser;
 
