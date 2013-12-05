7d52987addb1d9a2e344efd09def23ef488a24c6
odedc <odedcagan@gmail.com>
2013-12-05 19:52:22 +0200

    Update place.js

--- routes/place.js
+++ routes/place.js
@@ -26,7 +26,7 @@ exports.SearchPlace = function(req, res)
     var url = require('url');
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
-    var placeName = query.placeName;
+    var placeName = query.name;
 	if (placeName)
 	{
 		dal.getPlaceByName(placeName, function(err, result)
