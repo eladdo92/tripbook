0f96a06a8fe1a8cc1e57b207e07834c239b28ed2
odedc <odedcagan@gmail.com>
2013-12-24 22:33:02 +0200

    Update register.js

--- public/js/register.js
+++ public/js/register.js
@@ -1,24 +1,13 @@
-$(document).on('pagebeforeshow', '#register', function(){  
-        $(document).on('click', '#submit', function() { // catch the form's submit event
-        if($('#username').val().length > 0 && $('#email').val().length > 0 && $('#password').val().length > 0){            
-            var user = {};
-            user.name = $('#username').val();
-            user.email = $('#email').val();
-            user.password = $('#password').val();    
+$(document).ready(function(){
+    $('#register').submit(function(event){
+        var user = {};
+        user.name = $('#username').val();
+        user.email = $(this).find('#email').val();
+        user.password = $(this).find('#password').val();
 
-            tripbookController.register(user);              
+        tripbookController.register(user);        
 
-        } else {
-            alert('Please fill all nececery fields');
-        }           
-            return false; //cancel original event to prevent form submitting
-        });    
+        event.preventDefault();
+        return false;
+    });
 });
-
-$(document).on('pagebeforeshow', '#second', function(){     
-    $('#second [data-role="content"]').append('This is a result of form submition: ' + resultObject.formSubmitionResult);  
-});
-
-var resultObject = {
-    formSubmitionResult : null  
-}
