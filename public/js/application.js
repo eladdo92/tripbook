62fbe69bd0db0f4a5a018a2fcc3c31a4c22e7045
Ben Hodeda <benh_1992@yahoo.com>
2014-01-10 20:40:24 +0200

    add geolocation

--- public/js/application.js
+++ public/js/application.js
@@ -27,6 +27,17 @@ $('#registerpage').live('pageinit',function(){
 });
 
 $('#posttrackpage').live('pageinit',function(){
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+            var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+            var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
+                + latlon + "&zoom=14&size=400x300&sensor=false";
+            document.getElementById("mapholder").innerHTML = "<img src='" + img_url + "'>";
+        });
+    }
+
     $('#posttrack').submit(function(event){
 
         var user = userManager.getCurrentUser();
